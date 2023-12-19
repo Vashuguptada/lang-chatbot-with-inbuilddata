c091ec2ea2ccc5e659b628439503029a2c03f61e
vashuguptada <vashugupta.da@gmail.com>
2023-12-19 19:31:57 +0530

    new chatbot running fine ascasjhfjhf

--- app.py
+++ app.py
@@ -3,11 +3,23 @@ import streamlit as st
 
 from utils import get_answer_csv
 
-st.header("Chat with any CSV")
-uploaded_file = st.file_uploader("Upload a csv file", type=["csv"])
+# Provide the path to the predefined CSV file
+predefined_csv_path = "sale.csv"
 
-if uploaded_file is not None:
-    query = st.text_area("Ask any question related to the document")
-    button = st.button("Submit")
-    if button:
-        st.write(get_answer_csv(uploaded_file, query))
+st.header("Chat with any CSV")
+# Load the predefined CSV file
+uploaded_file = open(predefined_csv_path, "rb")
+
+query = st.text_area("Ask any question related to the document")
+button = st.button("Submit")
+
+if button:
+    st.write(get_answer_csv(uploaded_file, query))
+
+# uploaded_file = st.file_uploader("Upload a csv file", type=["csv"])
+
+# if uploaded_file is not None:
+#     query = st.text_area("Ask any question related to the document")
+#     button = st.button("Submit")
+#     if button:
+#         st.write(get_answer_csv(uploaded_file, query))
