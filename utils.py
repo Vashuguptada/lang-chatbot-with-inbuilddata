4b86235c02a9244fccf1db7ebad9298e58e0bf10
vashuguptada <vashugupta.da@gmail.com>
2023-12-20 12:31:46 +0530

    second

--- utils.py
+++ utils.py
@@ -12,18 +12,7 @@ from langchain.llms import OpenAI
 
 
 def get_answer_csv(file_path: str, query: str) -> str:
-    """
-    Returns the answer to the given query by querying a CSV file.
 
-    Args:
-    - file (str): the file path to the CSV file to query.
-    - query (str): the question to ask the agent.
-
-    Returns:
-    - answer (str): the answer to the query from the CSV file.
-    """
-    # Load the CSV file as a Pandas dataframe
-    # df = pd.read_csv(file)
     df = pd.read_csv("sale.csv")
 
     # Create an agent using OpenAI and the Pandas dataframe
